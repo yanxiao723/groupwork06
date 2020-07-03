bfcd436131591c7cb5594d195c0b1f35d6be5aab
group 6 taotao <1968719597@qq.com>
2020-07-03 09:22:14 +0800

    this is our first try

--- final1/data_deal.py
+++ final1/data_deal.py
@@ -1,6 +1,12 @@
+"""
+@author:Delin Fan
+ID:320180939721
+"""
+
+
 import matplotlib.pyplot as plt
 
 def data_predealing(clean_list: list):
-    x = [i for i in range(24)]
-    plt.plot(x, clean_list)
+    x = [i for i in range(24)]#j建立列表
+    plt.plot(x, clean_list)#绘制出新数据的图
     plt.show()
